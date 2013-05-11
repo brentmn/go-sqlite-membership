131135befac5d94c2457dfd17408a85fd8f04957
Brent Anderson <brentmn@gmail.com>
2013-05-10 21:17:36 -0500

    removed debug from src and added to ignored .notes

--- src/membership/crud.go
+++ src/membership/crud.go
@@ -1,12 +1,8 @@
 package main
 
 import (
-	//"database/sql"
 	"errors"
 	"fmt"
-
-//_ "github.com/mattn/go-sqlite3"
-//"os"
 )
 
 func (this *Db) Get(id int64) (Registration, error) {
@@ -143,109 +139,3 @@ func (this *Db) Delete(id int64) error {
 
 	return err
 }
-
-/*
-
-	os.Remove("data/membership.db")
-
-	db := Db{"brent", nil}
-
-	//db, err := sql.Open("sqlite3", "data/membership.db")
-
-	err := db.sCheck()
-
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer db.Close()
-
-	sqls := []string{
-		"create table registration (id integer not null primary key, email text, password text)",
-		"delete from registration",
-	}
-	for _, sql := range sqls {
-		_, err = db.Exec(sql)
-		if err != nil {
-			fmt.Printf("%q: %s\n", err, sql)
-			return
-		}
-	}
-
-	tx, err := db.Begin()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	stmt, err := tx.Prepare("insert into registration(id, email, password) values(?, ?, ?)")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer stmt.Close()
-	for i := 0; i < 100; i++ {
-		_, err = stmt.Exec(i, fmt.Sprintf("こんにちわ世界%03d", i), "na")
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-	}
-	tx.Commit()
-
-	rows, err := db.Query("select id, email, password from registration")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer rows.Close()
-	for rows.Next() {
-		var id int
-		var email string
-		var password string
-		rows.Scan(&id, &email, &password)
-		fmt.Println(id, email, password)
-	}
-	rows.Close()
-
-	stmt, err = db.Prepare("select email from registration where id = ?")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer stmt.Close()
-	var name string
-	err = stmt.QueryRow("3").Scan(&name)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println(name)
-
-	_, err = db.Exec("delete from registration")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	_, err = db.Exec("insert into registration(id, email, password) values(1, 'foo@bar.com', 'pass'), (2, 'bar@foo.com', 'pass'), (3, 'baz@baz.com', 'pass')")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	rows, err = db.Query("select id, email, password from registration")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer rows.Close()
-	for rows.Next() {
-		var id int
-		var email string
-		var password string
-		rows.Scan(&id, &email, &password)
-		fmt.Println(id, email, password)
-	}
-	rows.Close()
-
-*/
